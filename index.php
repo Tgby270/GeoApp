b54fb25dc0347768a5ef456586354b61e8ef3f34
Akumaa <valoumar0711@gmail.com>
2025-11-14 09:11:34 +0100

    maybe repaired

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- Load site-wide styles so elements like #page-title are styled -->
+    <link rel="stylesheet" href="/GeoApp/CSS/style.css">
 </head>
 
 <body>
@@ -12,7 +14,7 @@
         <?php include 'HTML/header.html'; ?>
     </header>
     
-    <h1>Bienvenue sur GeoApp</h1>
+    <h1 id="page-title">Bienvenue sur GeoApp !</h1>
 
 </body>
 
