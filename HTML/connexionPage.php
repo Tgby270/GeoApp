22ffcb211708d2df9c131acc7a7d16bcd8c4937c
EthanCoombes <ethan.d.coombes@gmail.com>
2025-11-16 13:17:07 +0100

    style inscription et connexion

--- HTML/connexionPage.php
+++ HTML/connexionPage.php
@@ -3,21 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/GeoApp/CSS/inscription.css">
     <title>Connexion</title>
 </head>
 <body>
     <header>
         <?php include './header.html'; ?>
     </header>
-    <form action="/GEOAPP/PHP/connexion.php" method="post">
-        <label for="siret">Siret : </label>
-        <input type="siret" id="siret" name="siret" required><br><br>
+    <div class="form-container">
+        <form action="/GEOAPP/PHP/connexion.php" method="post">
+            <label for="siret">Siret : </label>
+            <input type="integer" id="siret" name="siret" required><br><br>
 
-        <label for="password">Mot de passe : </label>
-        <input type="password" id="password" name="password" required><br><br>
+            <label for="password">Mot de passe : </label>
+            <input type="password" id="password" name="password" required><br><br>
 
-        <input type="submit" value="Se connecter">
-        <a href="/GeoApp/HTML/inscription.php">Pas de compte ? Inscrivez-vous ici.</a>
-    </form>
+            <input type="submit" value="Se connecter">
+            <br>
+            <a href="/GeoApp/HTML/inscription.php">Pas de compte ? Inscrivez-vous ici.</a>
+        </form>
+    </div>
 </body>
 </html>
--- HTML/inscription.php
+++ HTML/inscription.php
@@ -3,27 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/GeoApp/CSS/inscription.css">
     <title>Inscription</title>
 </head>
 <body>
     <header>
         <?php include './header.html'; ?>
     </header>
-    <form action="/GEOAPP/PHP/inscription.php" method="post">
-        <label for="siret">Numero de Siret : </label>
-        <input type="integer" id="siret" name="siret" required><br><br>
+    <div class="form-container">
+        <form action="/GEOAPP/PHP/inscription.php" method="post">
+            <label for="siret">Numero de Siret : </label>
+            <input type="integer" id="siret" name="siret" required><br><br>
 
-        <label for="email">Email : </label>
-        <input type="email" id="email" name="email" required><br><br>
+            <label for="email">Email : </label>
+            <input type="email" id="email" name="email" required><br><br>
 
-        <label for="password">Mot de passe :</label>
-        <input type="password" id="password" name="password" required><br><br>
+            <label for="password">Mot de passe :</label>
+            <input type="password" id="password" name="password" required><br><br>
 
-        <label for="password2">Vérification mot de passe :</label>
-        <input type="password2" id="password2" name="password2" required><br><br>
+            <label for="password2">Vérification mot de passe :</label>
+            <input type="password2" id="password2" name="password2" required><br><br>
 
-        <input type="submit" value="S'inscrire">
-        <a href="/GeoApp/HTML/connexionPage.php">Déjà un compte ? Connectez-vous ici.</a>
-    </form>
+            <input type="submit" value="S'inscrire">
+            <br>
+            <a href="/GeoApp/HTML/connexionPage.php">Déjà un compte ? Connectez-vous ici.</a>
+        </form>
+    </div>
 </body>
 </html>
